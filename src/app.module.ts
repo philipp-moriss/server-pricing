dfe357e03f988840ccd04c592c519eeb22000221
philipp-moriss <filverem@gmail.com>
2022-08-17 22:10:08 +0300

    add module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TestingRouteController } from './testing-route/testing-route.controller';
+import { WalletModule } from './wallet/wallet.module';
+import { UserModule } from './user/user.module';
+import { SpendingModule } from './spending/spending.module';
+import { AuthModule } from './auth/auth.module';
+import { WalletModule } from './wallet/wallet.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [WalletModule, AuthModule, SpendingModule, UserModule],
+  controllers: [AppController, TestingRouteController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-	return 'Hello World!';
+    return 'Hello World!';
+  }
+
+  getTestingMock(): string {
+    return JSON.stringify([{ wallet: 'test', currency: 'usd' }]);
   }
 }
