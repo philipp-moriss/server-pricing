1cff4b7f1214c34e1df87b494cd737e07e28bfdf
dotSality <90727519+dotSality@users.noreply.github.com>
2022-09-21 22:16:07 +0200

    auth guards

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Headers, HttpException, HttpStatus, UseGuards } from "@nestjs/common";
+import { Controller, Get, Headers, UseGuards } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { UserModel } from "./user.model";
 import { JWTService } from "../auth/services/jwt.service";
@@ -13,9 +13,6 @@ export class UserController {
   @UseGuards(AuthGuard)
   @Get("/user")
   getUserById(@Headers("authorization") token: string): Promise<UserModel | null> {
-    if (!token) {
-      throw new HttpException("You are not authorized", HttpStatus.UNAUTHORIZED);
-    }
     const jwtPayload = this.jwtService.checkTokenExpiry(token);
     if (jwtPayload) {
       const { _id } = jwtPayload;
