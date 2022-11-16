0413ead4b20ca9c7dc87e3f9991ca19aee17b9c9
p.veremeichik <p.veremeichik@richbrains.net>
2022-11-16 11:51:57 +0300

    add required params

--- src/spending/spending.model.ts
+++ src/spending/spending.model.ts
@@ -8,6 +8,7 @@ export class SpendingModel extends Document{
   @Prop({
     type: () => String,
     nullable: false,
+    required: true,
   })
   title: string;
 
@@ -27,22 +28,25 @@ export class SpendingModel extends Document{
   @Prop({
     type: () => String,
     nullable: true,
+    required: true,
   })
   description: string;
   @Prop({
     type: () => String,
-    nullable: true,
+    required: true,
   })
   category: string;
   @Prop({
     type: () => Number,
     nullable: false,
+    required: true,
   })
   amount: number;
 
   @Prop({
     type: () => String,
     nullable: false,
+    required: true,
   })
   currency: string;
 
