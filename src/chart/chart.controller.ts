d12c27c2bca4603c7876698ba862f37a2c97974d
Nick <shuminskiy23@gmail.com>
2022-12-20 01:12:43 +0300

    work over mobile chart data

--- src/chart/chart.controller.ts
+++ src/chart/chart.controller.ts
@@ -1,7 +1,7 @@
 import {Controller, Get, HttpException, HttpStatus, Query, UseGuards} from '@nestjs/common';
 import {SpendingService} from "../spending/spending.service";
 import {User} from "../decarators/user.decarator";
-import {ChartDataset, ChartService} from "./chart.service";
+import {ChartDataset, ChartDatasetMobile, ChartService} from "./chart.service";
 import {AuthGuard} from "../guards/auth.guard";
 import {getChartDataDto} from "./dto/chart.dto";
 
@@ -32,6 +32,26 @@ export class ChartController {
             throw new HttpException('userId not correct', HttpStatus.BAD_REQUEST);
         }
         const dataSetForChart = await this.chartService.getChartDataset(allHistory)
+        return dataSetForChart
+    }
+    @Get('/getChartDataMobile')
+    async getChartDataMobile(@User('_id') userId: string, @Query() queryParams: getChartDataDto): Promise<ChartDatasetMobile[] | null> {
+
+        const currentYear = Number(queryParams.year ?? new Date().getFullYear())
+
+        const paramsForSearchSpending = {
+            createdAt: {
+                $gte: new Date(currentYear, 1, 1),
+                $lt: new Date(currentYear, 12, 31)
+            },
+            userId,
+            walletId: queryParams.walletId
+        }
+        const allHistory = await this.spendingService.getSpendingByParameters(paramsForSearchSpending);
+        if (!allHistory) {
+            throw new HttpException('userId not correct', HttpStatus.BAD_REQUEST);
+        }
+        const dataSetForChart = await this.chartService.getChartDatasetForMobile(allHistory)
         return dataSetForChart
     }
 
--- src/chart/chart.service.ts
+++ src/chart/chart.service.ts
@@ -1,26 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
 import {SpendingModel} from "../spending/spending.model";
 
 
 type DatasetData = {
-    x : string,
-    y : string,
+    x: string,
+    y: string,
 }
 
 export type ChartDataset = {
-    label : string,
-    data : DatasetData[]
+    label: string,
+    data: DatasetData[]
 }
-
+export type ChartDatasetMobile = {
+    name: string,
+    population: number,
+    color: string,
+    legendFontColor: string,
+}
 
 @Injectable()
 export class ChartService {
 
-    async getChartDataset (spendingArray : SpendingModel[]) : Promise<ChartDataset[]> {
+    async getChartDataset(spendingArray: SpendingModel[]): Promise<ChartDataset[]> {
         const datasets = [];
-        const transformToChartData = spendingArray.reduce((accState: { [key: string] : string}, currItem) => {
+        const transformToChartData = spendingArray.reduce((accState: { [key: string]: string }, currItem) => {
             const currCategoryKey = `${currItem.category}//${currItem.walletName}//${currItem.currency}//${currItem.walletId}`;
-            const currMonth = currItem.createdAt.toLocaleString('default', { month: 'long' });
+            const currMonth = currItem.createdAt.toLocaleString('default', {month: 'long'});
             if (!accState[currCategoryKey]) {
                 accState[currCategoryKey] = <string>{};
             }
@@ -38,14 +43,50 @@ export class ChartService {
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-ignore
             for (const dataKey in transformToChartData[transformToChartDataKey]) {
-                data.push({ x: dataKey, y: transformToChartData[transformToChartDataKey][dataKey] });
+                data.push({x: dataKey, y: transformToChartData[transformToChartDataKey][dataKey]});
             }
             const splitKey = transformToChartDataKey.split('//')
             const label = `Category: ${splitKey[0]}, "${splitKey[2]}"`
-            datasets.push({ label: label, data: data });
+            datasets.push({label: label, data: data});
         }
 
         return datasets
+    }
+
+    async getChartDatasetForMobile(spendingArray: SpendingModel[]): Promise<ChartDatasetMobile[]> {
+        const generateColor = () => {
+            const randomColor = Math.floor(Math.random() * 16777215)
+                .toString(16)
+                .padStart(6, '0');
+            return `#${randomColor}`;
+        };
+        const datasetsMobile = [];
+        const transformToChartData = spendingArray.reduce((accState: { [key: string]: number }, currItem) => {
+            const currCategoryKey = `${currItem.category}/${currItem.currency}`;
+            const currMonth = currItem.createdAt.toLocaleString('default', {month: 'long'});
+            if (!accState[currCategoryKey]) {
+                accState[currCategoryKey] = 0
+            }
+            if (accState[currCategoryKey]) {
+                accState[currCategoryKey] =
+                    accState[currCategoryKey] + Number(currItem.amount);
+            } else {
+                accState[currCategoryKey] = Number(currItem.amount)
+            }
+            return accState;
+        }, {});
+
+        for (const transformToChartDataKey in transformToChartData) {
+            const splitKey = transformToChartDataKey.split('/')
+            const label = `${splitKey[0]}, "${splitKey[1]}"`
+            datasetsMobile.push({
+                label: label,
+                population: transformToChartData[transformToChartDataKey],
+                color: generateColor(),
+                legendFontColor: generateColor()
+            });
+        }
+        return datasetsMobile
     }
 
 }
